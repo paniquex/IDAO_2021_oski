0e1082981f3d401fd3523b5cab054e861e74cc13
paniquex <paniquexx@gmail.com>
2021-03-22 23:14:42 +0300

    dummy paths were added

--- src/paths.py
+++ src/paths.py
@@ -1,16 +1,1 @@
-DATA_ROOT = "/media/paniquex/samsung_2tb/rfcx_kaggle/rfcx-species-audio-detection"
-
-TRAIN_RAW = "/media/paniquex/samsung_2tb/rfcx_kaggle/rfcx-species-audio-detection/train"
-TEST_RAW = "/media/paniquex/samsung_2tb/rfcx_kaggle/rfcx-species-audio-detection/test"
-
-TRAIN_TF = "/media/paniquex/samsung_2tb/rfcx_kaggle/rfcx-species-audio-detection/tfrecords/train"
-TEST_TF = "/media/paniquex/samsung_2tb/rfcx_kaggle/rfcx-species-audio-detection/tfrecords/test"
-
-TRAIN_22K = "/media/paniquex/samsung_2tb/rfcx_kaggle/rfcx-species-audio-detection/train_22050"
-TEST_22K = "/media/paniquex/samsung_2tb/rfcx_kaggle/rfcx-species-audio-detection/test_22050"
-
-TRAIN_32K = "/media/paniquex/samsung_2tb/rfcx_kaggle/rfcx-species-audio-detection/train_32000"
-TEST_32K = "/media/paniquex/samsung_2tb/rfcx_kaggle/rfcx-species-audio-detection/test_32000"
-
-TRAIN_48K = "/media/paniquex/samsung_2tb/rfcx_kaggle/rfcx-species-audio-detection/train_48000"
-TEST_48K = "/media/paniquex/samsung_2tb/rfcx_kaggle/rfcx-species-audio-detection/test_48000"
+# dummy
